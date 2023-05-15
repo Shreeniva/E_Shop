4e9f777e1b105c9e468bd6bd9c0bddb76469fd97
sandhiya.s <sandhiya_s@vttech.in>
2023-05-15 19:00:02 +0530

    card design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './component';
 import HomePage from './component/Pages/HomePage';
 import Screen2 from './component/Pages/Screen2';
+import ProductCard from '../src/component/Pages/product/productCard'
+
 const App = () => {
   return (
     <Router>
@@ -10,6 +12,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={HomePage} />
          <Route path="/cart" component={Screen2} />
+         <Route path="/productCard" component={ProductCard} />
         {/*<Route path="/route2" component={Route2} /> */}
       </Switch>
     </Router>
--- src/component/Pages/HomePage.js
+++ src/component/Pages/HomePage.js
@@ -2,9 +2,11 @@ import React from 'react'
 import MovingCards from './MovingCards';
 import SliderImages from '../Slider';
 import { Card, Grid } from '@mui/material';
+import ProductCard from './product/productCard';
 
 function HomePage() {
    return (
+    <div style={{margin:"40px"}}>
          <Grid style={{ display: 'flex', justifyContent: 'center', padding: 10 ,backgroundColor:'#fff0db'}}>
             <Card elevation={15}>
                 {/* <div style={{ display: 'flex' }}>
@@ -22,12 +24,13 @@ function HomePage() {
                      <SliderImages pageType="thirdSlider"/>
                      </Grid>
                   </Grid>
-                
-                
-                
                 </div>
             </Card>
         </Grid>
+        <div style={{ marginTop:"15px", marginBottom:"200px"}}>
+            <ProductCard/>
+        </div>
+        </div>
    )
 }
 export default HomePage
